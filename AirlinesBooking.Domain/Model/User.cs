2a95bf1d76d1d730cae73c46dc67139213e2a5d7
Huseynjon Haydarov <huseynhaydarov0@gmail.com>
2023-03-10 17:13:24 +0500

    Add project files.

---
+++ AirlinesBooking.Domain/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlinesBooking.Domain.Model
+{
+    internal class User: EntityBase
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+}
